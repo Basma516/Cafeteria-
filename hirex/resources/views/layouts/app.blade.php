9bef8430b50b5cb2d5f46ba36662b6d11c2035a9
shrouk <shroukeslam676@gmail.com>
2024-08-30 20:06:13 +0300

    changes 2

--- hirex/resources/views/layouts/app.blade.php
+++ hirex/resources/views/layouts/app.blade.php
@@ -137,60 +137,7 @@
 </style>
 
 <body>
-    <div id="app">
-        <!-- Navbar Area Start -->
-
-         <nav class="navbar navbar-expand-lg navbar-dark">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contact Us</a>
-                    </li>
-                </ul>
-
-                <ul class="navbar-nav">
-                    @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="login"><i class="fas fa-sign-in-alt"></i> Login</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="register"><i class="fas fa-user-plus"></i> Signup</a>
-                        </li>
-                        @endguest @auth
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i
-                                    class="fas fa-user"></i>
-                                {{ Auth::user()->first_name }}
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ url('dashboard') }}"><i class="far fa-id-badge"></i></i>
-                                    Profile</a>
-                                <a class="dropdown-item" href="{{ url('contactm') }}"><i
-                                        class="fas fa-envelope-open-text"></i> Messages</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="{{ url('logout') }}"><i class="fas fa-sign-out-alt"></i>
-                                    Logout</a>
-                            </div>
-                        </li>
-                    @endauth
-                </ul>
-            </div>
-        </nav> 
-
+  
 
         <main class="py-4 container">
             @yield('content')
