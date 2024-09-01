90fbffbdf25ceadadd30ea11d31ec20c9bc9d4ed
rawansaied <90210323+rawansaied@users.noreply.github.com>
2024-09-01 19:02:39 +0300

    Updates on employer model

--- hirex/routes/web.php
+++ hirex/routes/web.php
@@ -3,6 +3,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployerController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\CandidateController;
 
 use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Auth;
@@ -24,40 +25,40 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard.index');
-});
-Route::get('/dashboard/candidate', function () {
-    return view('dashboard.candidate');
-})->name('candidate');
-Route::get('/dashboard/employer', function () {
-    return view('dashboard.employer');
-})->name('employer');
-Route::get('/dashboard/jobs', function () {
-    return view('dashboard.jobs');
-})->name('jobs');
-Route::get('/dashboard/category', function () {
-    return view('dashboard.category');
-})->name('category');
-Route::get('/dashboard/candidate/edit', function () {
-    return view('dashboard.candidate.edit');
-})->name('updateCandidate');
+// Route::get('/dashboard', function () {
+//     return view('dashboard.index');
+// });
+// Route::get('/dashboard/candidate', function () {
+//     return view('dashboard.candidate');
+// })->name('candidate');
+// Route::get('/dashboard/employer', function () {
+//     return view('dashboard.employer');
+// })->name('employer');
+// Route::get('/dashboard/jobs', function () {
+//     return view('dashboard.jobs');
+// })->name('jobs');
+// Route::get('/dashboard/category', function () {
+//     return view('dashboard.category');
+// })->name('category');
+// Route::get('/dashboard/candidate/edit', function () {
+//     return view('dashboard.candidate.edit');
+// })->name('updateCandidate');
 
-Route::get('/dashboard/employer/edit', function () {
-    return view('dashboard.employer.edit');
-})->name('updateEmployer');
+// Route::get('/dashboard/employer/edit', function () {
+//     return view('dashboard.employer.edit');
+// })->name('updateEmployer');
 
-Route::get('/dashboard/category/add', function () {
-    return view('dashboard.category.add');
-})->name('addCategory');
+// Route::get('/dashboard/category/add', function () {
+//     return view('dashboard.category.add');
+// })->name('addCategory');
 
-Route::get('/dashboard/category/edit', function () {
-    return view('dashboard.category.edit');
-})->name('editCategory');
+// Route::get('/dashboard/category/edit', function () {
+//     return view('dashboard.category.edit');
+// })->name('editCategory');
 
-Route::get('/dashboard/job/view', function () {
-    return view('dashboard.jobs.view');
-})->name('jobView');
+// Route::get('/dashboard/job/view', function () {
+//     return view('dashboard.jobs.view');
+// })->name('jobView');
 
 Auth::routes();
 
@@ -67,28 +68,28 @@ Route::view('/profile', 'users.edit_profile')->name('profile');
 
 
 // Dashboard routes for different user types
-Route::get('/employer/dashboard', [EmployerController::class, 'dashboard'])->name('employer.dashboard');
-Route::get('/candidate/dashboard', [CandidateController::class, 'dashboard'])->name('candidate.dashboard');
-Route::get('/admin/dashboard', [AdminController::class, 'dashboard']);
+// Route::get('/employer/dashboard', [EmployerController::class, 'dashboard'])->name('employer.dashboard');
+// Route::get('/candidate/dashboard', [CandidateController::class, 'dashboard'])->name('candidate.dashboard');
+// Route::get('/admin/dashboard', [AdminController::class, 'dashboard']);
 
- Route::get('/job', function () {
-     return view('jobs.show');
- });
+//  Route::get('/job', function () {
+//      return view('jobs.show');
+//  });
 
- Route::get('/category', function () {
-     return view('jobs.jobbycategory'); 
- });
+//  Route::get('/category', function () {
+//      return view('jobs.jobbycategory'); 
+//  });
 
 
 
- Route::get('/all-jobs', function () {
-    return view('jobs.alljobs'); 
- })->name('alljobs');
+//  Route::get('/all-jobs', function () {
+//     return view('jobs.alljobs'); 
+//  })->name('alljobs');
 
 
 // routes/web.php
 
-Route::view('/create-job', 'jobs.createjob');
+// Route::view('/create-job', 'jobs.createjob');
 
 
 Route::resource('employers', EmployerController::class);
